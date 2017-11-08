ab1686a7576290dd09fa74a0d124fb8d29f8710c
espinozamaria <phillyswirl002@mail.fresnostate.edu>
2017-11-08 11:11:14 -0800

    Added basic login modal - not yet synced to db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { ConfirmComponent } from './confirm/confirm.component';
+import { DialogService } from 'ng2-bootstrap-modal';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-}
+  constructor(private dialogService:DialogService) {}
+    showConfirm() {
+    let disposable = this.dialogService.addDialog(ConfirmComponent, {
+    
+    })
+    // This would be where the information for the user login would be placed
+    // if isConfirmed{
+        // check to make sure that user is registered in the db
+        // else make them register
+    //}
+    .subscribe((isConfirmed)=>{
+      //We get the dialog result
+      // if(isConfirmed){
+      //   alert('accepted');
+      // }
+      // else{
+      //   alert('declined');
+      // }
+      });
+    }
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RoutingModule} from './app.routes';
+import { BootstrapModalModule } from 'ng2-bootstrap-modal';
+import { LoginComponent } from './login/login.component';
+import { ConfirmComponent } from './confirm/confirm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ConfirmComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BootstrapModalModule,
+    
+    // Routing modules should be the last thing on the list, otherwise bad things happen
+    // these modules are loaded in order
+    RoutingModule
+  ],
+        //Don't forget to add the component to entryComponents section
+  entryComponents: [
+    ConfirmComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
