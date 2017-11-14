b8b35528aa3deb564a13f62f3fa15da9124fd88e
Yesenia <eliyessie@gmail.com>
2017-11-13 20:21:44 -0800

    footer design, button not functional and need to link the footer component

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+// THIS IS THE JAVASCRIPT FOR THE BUTTON
+// function showDiv() {
+//     var x = document.getElementById("displayFooter");
+//     if (x.style.display === "block") {
+//         x.style.display = "none";
+//     } else {
+//         x.style.display = "block";
+//     }
+// }
