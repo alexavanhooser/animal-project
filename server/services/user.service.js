9c6963a4e7c6dcf7f1be5bb6fd7a38d5462daf15
espinozamaria <phillyswirl002@mail.fresnostate.edu>
2017-11-27 00:06:01 -0800

    Working server... needs interaction with adding users in db

--- server/services/user.service.js
+++ server/services/user.service.js
@@ -92,7 +92,7 @@ function create(userParam) {
         var user = _.omit(userParam, 'password');
  
         // add hashed password to user object
-        user.hash = bcrypt.hashSync(userParam.password, 10);
+        // user.hash = bcrypt.hashSync(userParam.password, 10);
  
         db.users.insert(
             user,
